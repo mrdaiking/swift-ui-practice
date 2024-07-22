362e6076add0e95bd7b4300e5cc89fc405ae0839
Cuong <cuong@blayn.co.jp>
2024-07-22 16:03:49 +0900

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Kỳ Nguyên Đại on 2024/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
